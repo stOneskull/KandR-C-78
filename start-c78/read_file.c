b909e273237cc3798a50259d09edd509caf11599
stOneskull <stOne@skull.casa>
2025-09-08 09:33:32 +1000

    read file

---
+++ start-c78/read_file.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    char line[1000];
+    FILE *hand;
+    hand = fopen("romeo.txt", "r");
+    while (fgets(line, 1000, hand) != NULL) {
+        printf("%s", line);
+    }
+    fclose(hand);
+    return 0;
+}
