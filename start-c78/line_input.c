03f3a9fbad170c3478a2d2f7738401c4faddd674
stOneskull <stOne@skull.casa>
2025-09-08 07:57:22 +1000

    line_input-safe

--- start-c78/line_input.c
+++ start-c78/line_input.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 
+/*
 int main() {
     char line[1000];
     puts("Enter line");
     scanf("%[^\n]1000s", line);
+    printf("Line: %s\n", line);
+    return 0;
+}
+*/
+
+/* safe */
+
+int main() {
+    char line[1000];
+    puts("Enter line");
+    fgets(line, 1000, stdin);
     printf("Line: %s\n", line);
     return 0;
 }
